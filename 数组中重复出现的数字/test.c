59f0a63ce3f988e8190f44ff4191e78e91f3e6cc
浅忆梦微凉 <3538922590@qq.com>
2018-10-07 16:58:40 +0800

    数组中重复出现的数字

--- 数组中重复出现的数字/test.c
+++ 数组中重复出现的数字/test.c
@@ -2,7 +2,7 @@
 #include<Windows.h>
 #include<stdio.h>
 #if 0
-//这种方法使用两个指针来遍历数组，运用两个for循环，时间复杂度为O（n^2）,空间复杂度为O(N)
+//这种方法使用两个指针来遍历数组，运用两个for循环，时间复杂度为O（n^2）,空间复杂度为O(1)
 void test()
 {
 	int array[7] = { 0, 3, 1, 0, 2, 5, 3 };
@@ -23,7 +23,8 @@ void test()
 		printf("没找到重复的数字\n");
 }
 #endif 
-
+#if 0
+//这种方法是先通过冒泡排序将数组排序，时间复杂度为O(n^2),空间复杂度为O(1);
 void test()
 {
 	int array[7] = { 2, 3, 1, 0, 2, 5, 3 };
@@ -50,7 +51,45 @@ void test()
 		}
 	}
 }
+#endif
+//这种方法使用折半插入排序对数组进行排序
+//比较次数虽然为nlog(n)(以2为底),但是时间复杂度任然为O(N^2);
 
+void test()
+{
+	int array[7] = { 0, 3, 1, 0, 2, 5, 3 };
+	int i = 2;
+	for (i = 2; i < sizeof(array) / sizeof(array[0]); i++)
+	{
+		int low = 1; int high = i - 1;
+		int temp = array[i];
+		while (low <= high)
+		{
+			int mid = (high - low) / 2 + low;
+			if (temp < array[mid])
+				high = mid - 1;
+			else
+				low = mid + 1;
+		}
+		//移动元素
+		int j = 0;
+		for (j = i - 1; j>=low; j--)
+		{
+			array[j + 1] = array[j];
+		}
+		array[low] = temp;
+	}
+	for (i = 0; i < sizeof(array)/sizeof(array[0])-1; i++)
+	{
+		if (array[i] == array[i + 1])
+		{
+			printf("重复出现的数字为：");
+			printf("%d ", array[i]);
+		}
+	}
+}
+//在提一种思路，如果数组中的元素已经排好序了，则它的元素应该与它的下标相同，可以根据这个
+//方法实现快速的排序，从而找到重复的数字，当然，这种方法只针对特殊情况。
 int main()
 {
 	test();
